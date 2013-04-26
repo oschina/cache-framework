70baf82aed92829d948dd503a34a6c806e2feed5
Winter Lau <oschina.net@gmail.com>
2013-04-26 23:16:14 +0800

    cc

--- src/net/oschina/common/cache/OSChinaCache.java
+++ src/net/oschina/common/cache/OSChinaCache.java
@@ -1,5 +1,8 @@
 package net.oschina.common.cache;
 
+import java.io.Serializable;
+import java.util.*;
+
 /**
  * 缓存入口
  * @author Winter Lau
@@ -9,14 +12,50 @@ public class OSChinaCache {
 	private final static OSCacheConfig config = OSCacheConfig.newInstance();
 	
 	public static void main(String[] args) throws Exception {		
-		CacheHolder cache = config.getCache("User");
-		
+		final CacheHolder cache = config.getCache("User");
 		long ct = System.currentTimeMillis();
-		for(int i=0;i<99999;i++)
-			cache.put(i, "Winter Lau #" + i);
+		/*
+		for(int i=0;i<100;i++){
+			new Thread(){{}
+			@Override
+			public void run() {
+				List<MyObject> objs = new ArrayList<MyObject>();
+				for(int i=0;i<100;i++){
+					objs.add(new MyObject(this.getName()+"#"+i, i));
+				}
+				cache.put(this.getName(), (Serializable)objs);
+			}}.start();
+		}
+		*/
 		System.out.println(System.currentTimeMillis()-ct);
-		System.out.println(cache + ":" + cache.get(12));
+		List<MyObject> objs = (List<MyObject>)cache.get("Thread-32");
+		for(MyObject obj : objs){
+			System.out.printf("name:%s,age:%d\n",obj.getName(),obj.getAge());
+		}
+		//System.out.println(cache + ":" + cache.get(12));
 		//cache.remove(12);
 	}
 
+	public static class MyObject implements Serializable {
+		private String name;
+		private int age;
+		private int age2;
+		public MyObject(){}
+		public MyObject(String name, int age){
+			this.name = name;
+			this.age = age;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+	}
 }
